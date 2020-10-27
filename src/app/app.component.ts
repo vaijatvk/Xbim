8bd7fd01cbd1788b61a604f56b37e13c731870b8
Vaijat Kokate <vaijatvk@gmail.com>
2020-10-27 19:40:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message, MessageType, Viewer } from '@xbim/viewer';
+import { Observable, of } from 'rxjs';
+import { map, catchError, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FlexViewer';
+  @ViewChild("canvas",{static:true}) canvas = ElementRef;
+  Viewer :Viewer;
+  constructor(private httpClient: HttpClient) { }
+ngOnInit(){
+
+  this.Viewer = new Viewer("canvas");
+  //this.Viewer.loadAsync("./assets/Files/956bf1bc-1963-4734-958f-748b1c4e640f.wexbim");
+  let accessToken = "";
+    this.getToken()
+    .subscribe((result: any) => {
+    accessToken = result;
+    console.log(result);
+    const headers: { [name: string]: string; } = { Authorization: "Bearer " + accessToken };
+  const url = "./assets/Files/956bf1bc-1963-4734-958f-748b1c4e640f.wexbim";
+    this.Viewer.loadAsync(url, url, headers, (msg: Message) => {
+      if (msg.type === MessageType.COMPLETED) {
+        this.Viewer.start(msg.wexbimId);
+      }
+      window['viewer'] = this.Viewer;
+  });
+  })
+  
 }
+// downloadFile(data: Blob) {
+//   const blob = new Blob([data], { type: 'application/octet-stream' });
+//   const url= window.URL.createObjectURL(blob);
+  
+// }
+
+getToken(): Observable<any> {
+  return this.httpClient.post("http://localhost/FlexMVC/Flex/GetToken",null).pipe(
+    map((body: any) => body.result),
+    catchError((error: any) => of(null))
+  );
+}
+
+}
+
